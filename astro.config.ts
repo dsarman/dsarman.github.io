7461d50ff526cc7d30c33ac4ba3a45145c41e928
David Šarman <contact@sarman.work>
2024-07-19 00:09:28 +0200

    fix: mark resvg as external

--- astro.config.ts
+++ astro.config.ts
@@ -37,6 +37,9 @@ export default defineConfig({
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
     },
+    ssr: {
+      external: ["@resvg/resvg-js"],
+    },
   },
   scopedStyleStrategy: "where",
   output: "server",
